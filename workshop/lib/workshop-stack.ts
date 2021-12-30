1bea380d2d28aaa482aa61432a45e35e4ebe42a4
Brad Schmitt <bschmitt@newrelic.com>
2021-12-30 13:40:28 -0600

    initial commit

---
+++ workshop/lib/workshop-stack.ts
@@ -0,0 +1,40 @@
+import { Duration, lambda_layer_awscli, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+
+export class WorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'
+    });
+
+    // const queue = new sqs.Queue(this, 'WorkshopQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+
+    // const topic = new sns.Topic(this, 'WorkshopTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+  }
+}
